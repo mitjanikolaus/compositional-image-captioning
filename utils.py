b8afb1a1f99f0e7de611dfe4cc3b335435a6d608
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2019-03-07 15:34:18 +0100

    Move constants to utils.py

--- utils.py
+++ utils.py
@@ -23,6 +23,61 @@ IMAGES_FILENAME = "images.hdf5"
 CAPTIONS_FILENAME = "captions.json"
 CAPTION_LENGTHS_FILENAME = "caption_lengths.json"
 
+NOUNS = "nouns"
+ADJECTIVES = "adjectives"
+
+PAIR_OCCURENCES = "pair_occurrences"
+NOUN_OCCURRENCES = "noun_occurrences"
+ADJECTIVE_OCCURRENCES = "adjective_occurrences"
+
+RELATION_NOMINAL_SUBJECT = "nsubj"
+RELATION_ADJECTIVAL_MODIFIER = "amod"
+RELATION_CONJUNCT = "conj"
+
+
+def contains_adjective_noun_pair(nlp_pipeline, caption, nouns, adjectives):
+    noun_is_present = False
+    adjective_is_present = False
+
+    doc = nlp_pipeline(caption)
+    sentence = doc.sentences[0]
+
+    for token in sentence.tokens:
+        if token.text in nouns:
+            noun_is_present = True
+        if token.text in adjectives:
+            adjective_is_present = True
+
+    dependencies = sentence.dependencies
+    caption_adjectives = {
+        d[2].text
+        for d in dependencies
+        if d[1] == RELATION_ADJECTIVAL_MODIFIER and d[0].text in nouns
+    } | {
+        d[0].text
+        for d in dependencies
+        if d[1] == RELATION_NOMINAL_SUBJECT and d[2].text in nouns
+    }
+    conjuncted_caption_adjectives = set()
+    for adjective in caption_adjectives:
+        conjuncted_caption_adjectives.update(
+            {
+                d[2].text
+                for d in dependencies
+                if d[1] == RELATION_CONJUNCT and d[0].text == adjective
+            }
+            | {
+                d[2].text
+                for d in dependencies
+                if d[1] == RELATION_ADJECTIVAL_MODIFIER and d[0].text == adjective
+            }
+        )
+
+    caption_adjectives |= conjuncted_caption_adjectives
+    combination_is_present = bool(adjectives & caption_adjectives)
+
+    return noun_is_present, adjective_is_present, combination_is_present
+
 
 def read_image(path):
     img = imread(path)
