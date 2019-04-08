59ebbb071cbb05f71ea1bd3e188e1cdef0f2edd2
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2019-04-08 16:17:42 +0200

    Add additional language encoding lstm

--- models/ranking_generating.py
+++ models/ranking_generating.py
@@ -8,7 +8,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 class RankGenEncoder(nn.Module):
-    DEFAULT_MODEL_PARAMS = {"image_features_size": 2048, "joint_embedding_size": 1024}
+    DEFAULT_MODEL_PARAMS = {"image_features_size": 2048, "joint_embeddings_size": 1024}
     DEFAULT_OPTIMIZER_PARAMS = {"encoder_learning_rate": 1e-4}
 
     def __init__(self, params):
@@ -16,7 +16,7 @@ class RankGenEncoder(nn.Module):
         self.params = update_params(self.DEFAULT_MODEL_PARAMS, params)
 
         self.image_emb = nn.Linear(
-            self.params["image_features_size"], self.params["joint_embedding_size"]
+            self.params["image_features_size"], self.params["joint_embeddings_size"]
         )
 
     def forward(self, image_features):
@@ -29,11 +29,11 @@ class RankGenDecoder(CaptioningModelDecoder):
     DEFAULT_MODEL_PARAMS = {
         "teacher_forcing_ratio": 1,
         "dropout_ratio": 0.0,
-        "joint_embedding_size": 1024,
-        "embeddings_size": 1000,
+        "joint_embeddings_size": 1024,
+        "word_embeddings_size": 1000,
         "attention_lstm_size": 1000,
         "attention_layer_size": 512,
-        "language_lstm_size": 1000,
+        "language_generation_lstm_size": 1000,
         "max_caption_len": 50,
         "fine_tune_decoder_embeddings": True,
     }
@@ -43,18 +43,20 @@ class RankGenDecoder(CaptioningModelDecoder):
         super(RankGenDecoder, self).__init__(word_map, params, pretrained_embeddings)
 
         self.attention_lstm = AttentionLSTM(
-            self.params["embeddings_size"],
-            self.params["language_lstm_size"],
-            self.params["joint_embedding_size"],
+            self.params["joint_embeddings_size"],
+            self.params["language_generation_lstm_size"],
             self.params["attention_lstm_size"],
         )
-        self.language_lstm = LanguageLSTM(
+        self.language_encoding_lstm = LanguageEncodingLSTM(
+            self.params["word_embeddings_size"], self.params["joint_embeddings_size"]
+        )
+        self.language_generation_lstm = LanguageGenerationLSTM(
             self.params["attention_lstm_size"],
-            self.params["joint_embedding_size"],
-            self.params["language_lstm_size"],
+            self.params["joint_embeddings_size"],
+            self.params["language_generation_lstm_size"],
         )
         self.attention = VisualAttention(
-            self.params["joint_embedding_size"],
+            self.params["joint_embeddings_size"],
             self.params["attention_lstm_size"],
             self.params["attention_layer_size"],
         )
@@ -64,46 +66,65 @@ class RankGenDecoder(CaptioningModelDecoder):
 
         # Linear layer to find scores over vocabulary
         self.fully_connected = nn.Linear(
-            self.params["language_lstm_size"], self.vocab_size, bias=True
+            self.params["language_generation_lstm_size"], self.vocab_size, bias=True
         )
 
         # linear layers to find initial states of LSTMs
-        self.init_h1 = nn.Linear(
-            self.params["joint_embedding_size"],
+        self.init_h_lan_enc = nn.Linear(
+            self.params["joint_embeddings_size"],
+            self.language_encoding_lstm.lstm_cell.hidden_size,
+        )
+        self.init_c_lan_enc = nn.Linear(
+            self.params["joint_embeddings_size"],
+            self.language_encoding_lstm.lstm_cell.hidden_size,
+        )
+        self.init_h_attention = nn.Linear(
+            self.params["joint_embeddings_size"],
             self.attention_lstm.lstm_cell.hidden_size,
         )
-        self.init_c1 = nn.Linear(
-            self.params["joint_embedding_size"],
+        self.init_c_attention = nn.Linear(
+            self.params["joint_embeddings_size"],
             self.attention_lstm.lstm_cell.hidden_size,
         )
-        self.init_h2 = nn.Linear(
-            self.params["joint_embedding_size"],
-            self.language_lstm.lstm_cell.hidden_size,
+        self.init_h_lan_gen = nn.Linear(
+            self.params["joint_embeddings_size"],
+            self.language_generation_lstm.lstm_cell.hidden_size,
         )
-        self.init_c2 = nn.Linear(
-            self.params["joint_embedding_size"],
-            self.language_lstm.lstm_cell.hidden_size,
+        self.init_c_lan_gen = nn.Linear(
+            self.params["joint_embeddings_size"],
+            self.language_generation_lstm.lstm_cell.hidden_size,
         )
 
     def init_hidden_states(self, encoder_output):
         v_mean = encoder_output.mean(dim=1)
 
-        h1 = self.init_h1(v_mean)
-        c1 = self.init_c1(v_mean)
-        h2 = self.init_h2(v_mean)
-        c2 = self.init_c2(v_mean)
-        states = [h1, c1, h2, c2]
+        h_lan_enc = self.init_h_lan_enc(v_mean)
+        c_lan_enc = self.init_c_lan_enc(v_mean)
+        h_attention = self.init_h_attention(v_mean)
+        c_attention = self.init_c_attention(v_mean)
+        h_lan_gen = self.init_h_lan_gen(v_mean)
+        c_lan_gen = self.init_c_lan_gen(v_mean)
+        states = [h_lan_enc, c_lan_enc, h_attention, c_attention, h_lan_gen, c_lan_gen]
 
         return states
 
     def forward_step(self, encoder_output, prev_words_embedded, states):
         v_mean = encoder_output.mean(dim=1)
-        h1, c1, h2, c2 = states
-        h1, c1 = self.attention_lstm(h1, c1, h2, v_mean, prev_words_embedded)
-        v_hat = self.attention(encoder_output, h1)
-        h2, c2 = self.language_lstm(h2, c2, h1, v_hat)
-        scores = self.fully_connected(self.dropout(h2))
-        states = [h1, c1, h2, c2]
+        h_lan_enc, c_lan_enc, h_attention, c_attention, h_lan_gen, c_lan_gen = states
+
+        h_lan_enc, c_lan_enc = self.language_encoding_lstm(
+            h_lan_enc, c_lan_enc, prev_words_embedded
+        )
+
+        h_attention, c_attention = self.attention_lstm(
+            h_attention, c_attention, h_lan_gen, v_mean, h_lan_enc
+        )
+        v_hat = self.attention(encoder_output, h_attention)
+        h_lan_gen, c_lan_gen = self.language_generation_lstm(
+            h_lan_gen, c_lan_gen, h_attention, v_hat
+        )
+        scores = self.fully_connected(self.dropout(h_lan_gen))
+        states = [h_lan_enc, c_lan_enc, h_attention, c_attention, h_lan_gen, c_lan_gen]
         return scores, states, None
 
     def beam_search(
@@ -127,21 +148,31 @@ class RankGenDecoder(CaptioningModelDecoder):
 
 
 class AttentionLSTM(nn.Module):
-    def __init__(self, dim_word_emb, dim_lang_lstm, dim_image_feats, hidden_size):
+    def __init__(self, joint_embeddings_size, dim_lang_lstm, hidden_size):
         super(AttentionLSTM, self).__init__()
         self.lstm_cell = nn.LSTMCell(
-            dim_lang_lstm + dim_image_feats + dim_word_emb, hidden_size, bias=True
+            2 * joint_embeddings_size + dim_lang_lstm, hidden_size, bias=True
         )
 
-    def forward(self, h1, c1, h2, v_mean, prev_words_embedded):
-        input_features = torch.cat((h2, v_mean, prev_words_embedded), dim=1)
+    def forward(self, h1, c1, h2, v_mean, h_lan_enc):
+        input_features = torch.cat((h2, v_mean, h_lan_enc), dim=1)
         h_out, c_out = self.lstm_cell(input_features, (h1, c1))
         return h_out, c_out
 
 
-class LanguageLSTM(nn.Module):
+class LanguageEncodingLSTM(nn.Module):
+    def __init__(self, word_embeddings_size, hidden_size):
+        super(LanguageEncodingLSTM, self).__init__()
+        self.lstm_cell = nn.LSTMCell(word_embeddings_size, hidden_size, bias=True)
+
+    def forward(self, h, c, prev_words_embedded):
+        h_out, c_out = self.lstm_cell(prev_words_embedded, (h, c))
+        return h_out, c_out
+
+
+class LanguageGenerationLSTM(nn.Module):
     def __init__(self, dim_att_lstm, dim_visual_att, hidden_size):
-        super(LanguageLSTM, self).__init__()
+        super(LanguageGenerationLSTM, self).__init__()
         self.lstm_cell = nn.LSTMCell(
             dim_att_lstm + dim_visual_att, hidden_size, bias=True
         )
