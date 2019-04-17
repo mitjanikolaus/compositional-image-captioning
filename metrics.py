Fix beam occurrences metric calculation

metrics.py
@@ -108,71 +108,78 @@ def calc_recall(
     return recall
 
 
-def beam_occurrences(generated_beams, beam_size, word_map, occurrences_data_file):
+def beam_occurrences(
+    generated_beams, beam_size, word_map, occurrences_data_files, max_print_length=20
+):
+    for occurrences_data_file in occurrences_data_files:
+        with open(occurrences_data_file, "r") as json_file:
+            occurrences_data = json.load(json_file)
 
-    with open(occurrences_data_file, "r") as json_file:
-        occurrences_data = json.load(json_file)
+        nouns = set(occurrences_data[NOUNS])
+        adjectives = set(occurrences_data[ADJECTIVES])
 
-    nouns = set(occurrences_data[NOUNS])
-    adjectives = set(occurrences_data[ADJECTIVES])
+        max_length = max([beams[-1].size(1) for beams in generated_beams.values()])
+        noun_occurrences = np.zeros(max_length)
+        adjective_occurrences = np.zeros(max_length)
+        pair_occurrences = np.zeros(max_length)
 
-    max_length = max([beams[-1].size(1) for beams in generated_beams])
-    noun_occurrences = np.zeros(max_length)
-    adjective_occurrences = np.zeros(max_length)
-    pair_occurrences = np.zeros(max_length)
+        num_beams = np.zeros(max_length)
 
-    num_beams = np.zeros(max_length)
+        _, _, test_indices = get_splits_from_occurrences_data([occurrences_data_file])
 
-    for beam in generated_beams:
-        for step, beam_timestep in enumerate(beam):
-            noun_match = False
-            adjective_match = False
-            pair_match = False
-            for branch in beam_timestep:
-                branch_words = set(decode_caption(branch.numpy(), word_map))
-                noun_occurs = bool(nouns & branch_words)
-                adjective_occurs = bool(adjectives & branch_words)
-                if noun_occurs:
-                    noun_match = True
-                if adjective_occurs:
-                    adjective_match = True
-                if noun_occurs and adjective_occurs:
-                    pair_match = True
-            if noun_match:
-                noun_occurrences[step] += 1
-            if adjective_match:
-                adjective_occurrences[step] += 1
-            if pair_match:
-                pair_occurrences[step] += 1
-            num_beams[step] += 1
+        for coco_id in test_indices:
+            beam = generated_beams[coco_id]
+            for step, beam_timestep in enumerate(beam):
+                noun_match = False
+                adjective_match = False
+                pair_match = False
+                for branch in beam_timestep:
+                    branch_words = set(decode_caption(branch.numpy(), word_map))
+                    noun_occurs = bool(nouns & branch_words)
+                    adjective_occurs = bool(adjectives & branch_words)
+                    if noun_occurs:
+                        noun_match = True
+                    if adjective_occurs:
+                        adjective_match = True
+                    if noun_occurs and adjective_occurs:
+                        pair_match = True
+                if noun_match:
+                    noun_occurrences[step] += 1
+                if adjective_match:
+                    adjective_occurrences[step] += 1
+                if pair_match:
+                    pair_occurrences[step] += 1
+                num_beams[step] += 1
 
-    print("Nouns: {}".format(noun_occurrences))
-    print("Adjectives: {}".format(adjective_occurrences))
-    print("Pairs: {}".format(pair_occurrences))
-    print("Number of beams: {}".format(num_beams))
+        print("Nouns: {}".format(noun_occurrences))
+        print("Adjectives: {}".format(adjective_occurrences))
+        print("Pairs: {}".format(pair_occurrences))
+        print("Number of beams: {}".format(num_beams))
 
-    # Print only occurrences up to timestep 20
-    print_length = min(20, len(np.trim_zeros(num_beams)))
+        # Print only occurrences up to max_print_length
+        print_length = min(max_print_length, len(np.trim_zeros(num_beams)))
 
-    steps = np.arange(print_length)
+        steps = np.arange(print_length)
 
-    plt.plot(
-        steps, noun_occurrences[:print_length] / num_beams[:print_length], label="nouns"
-    )
-    plt.plot(
-        steps,
-        adjective_occurrences[:print_length] / num_beams[:print_length],
-        label="adjectives",
-    )
-    plt.plot(
-        steps, pair_occurrences[:print_length] / num_beams[:print_length], label="pairs"
-    )
-    plt.legend()
-    plt.xlabel("timestep")
-    plt.title("Recall@{} in the decoding beam".format(beam_size))
-    plt.show()
-
-    return pair_occurrences
+        plt.plot(
+            steps,
+            noun_occurrences[:print_length] / num_beams[:print_length],
+            label="nouns",
+        )
+        plt.plot(
+            steps,
+            adjective_occurrences[:print_length] / num_beams[:print_length],
+            label="adjectives",
+        )
+        plt.plot(
+            steps,
+            pair_occurrences[:print_length] / num_beams[:print_length],
+            label="pairs",
+        )
+        plt.legend()
+        plt.xlabel("timestep")
+        plt.title("Recall@{} in the decoding beam".format(beam_size))
+        plt.show()
 
 
 def recall_captions_from_images(embedded_images, embedded_captions, testing_indices):