Fix average recall, make script for plotting

plot_recall_results.py
@@ -1,3 +1,6 @@
+import sys
+
+import argparse
 import json
 
 import numpy as np
@@ -7,34 +10,22 @@ from metrics import average_recall
 
 MODE_VERBOSE = "VERBOSE"
 MODE_AVERAGE_FOR_PAIR = "AVERAGE_FOR_PAIRS"
-MODE_AVERAGE_ALL = "AVERAGE_ALL"
 
-eval_files = [
-    "../eval/eval_checkpoint_BOTTOM_UP_TOP_DOWN_heldout_pairs_none_best.json",
-    "../eval/eval_checkpoint_BOTTOM_UP_TOP_DOWN_heldout_pairs_glove_best.json",
-    "../eval/eval_checkpoint_BOTTOM_UP_TOP_DOWN_RANKING_heldout_pairs_continued_joint_best.json",
-]
-mode = MODE_AVERAGE_ALL
-eval_datas = []
 
-for file in eval_files:
-    with open(file, "r") as json_file:
-        eval_data = json.load(json_file)
+def plot_recall_results(eval_files, mode, labels):
+    eval_datas = []
 
-    eval_datas.append(eval_data)
+    for file in eval_files:
+        with open(file, "r") as json_file:
+            eval_data = json.load(json_file)
 
-if mode == MODE_AVERAGE_ALL:
+        eval_datas.append(eval_data)
 
+    print("Average recall values:")
     for j, eval_data in enumerate(eval_datas):
-        true_positives = 0
-        numbers = 0
+        print(average_recall(eval_data))
 
-        recall = average_recall(eval_data)
-        print(recall)
-
-
-else:
-    fig, axes = plt.subplots(nrows=len(eval_datas[0]), sharex=True)
+    fig, axes = plt.subplots(nrows=len(eval_datas[0]), sharex=True, figsize=(8, 15))
     plt.suptitle("Recall")
     bar_width = 0.2
     if mode == MODE_VERBOSE:
@@ -47,7 +38,7 @@ else:
                 recall = np.array(
                     list(eval_data[pair]["true_positives"].values())
                 ) / np.array(list(eval_data[pair]["numbers"].values()))
-                rects1 = axis.bar(index + j * bar_width, recall, bar_width)
+                axis.bar(index + j * bar_width, recall, bar_width)
 
             axis.set_ylim(0, 1)
             axis.set_title(pair)
@@ -63,7 +54,7 @@ else:
                     list(eval_data[pair]["true_positives"].values())
                 ) / np.sum(list(eval_data[pair]["numbers"].values()))
 
-                rects1 = axis.bar(j * bar_width, recall, bar_width)
+                axis.bar(j * bar_width, recall, bar_width)
 
                 axis.text(x=j * bar_width, y=0.8, s=np.round(recall, 2), size=5)
             axis.set_ylim(0, 1)
@@ -73,13 +64,34 @@ else:
     # Common ylabel
     fig.text(0.06, 0.5, "Recall", ha="center", va="center", rotation="vertical")
     # Common legend
-    fig.legend(
-        axes,
-        labels=["upper bound", "glove embeddings", "joint objective"],
-        loc="upper right",
-        borderaxespad=0.1,
-    )
+    if labels:
+        fig.legend(labels=labels, loc="upper right", borderaxespad=0.1)
 
     plt.subplots_adjust(hspace=0.5)
 
     plt.show()
+
+
+def check_args(args):
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--eval-files", nargs="+", help="Files containing evaluation statistics"
+    )
+    parser.add_argument(
+        "--mode",
+        help="Mode",
+        default=MODE_AVERAGE_FOR_PAIR,
+        choices=[MODE_AVERAGE_FOR_PAIR, MODE_VERBOSE],
+    )
+    parser.add_argument(
+        "--labels", nargs="+", help="Labels for each model that was evaluated"
+    )
+    parsed_args = parser.parse_args(args)
+    print(parsed_args)
+    return parsed_args
+
+
+if __name__ == "__main__":
+    parsed_args = check_args(sys.argv[1:])
+    plot_recall_results(parsed_args.eval_files, parsed_args.mode, parsed_args.labels)