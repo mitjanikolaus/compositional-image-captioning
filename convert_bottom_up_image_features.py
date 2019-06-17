51ed5fe466dd22c35c983352ccb9fbbebee08d1f
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2019-06-17 14:35:15 +0200

    Convert bottom-up features without filtering coco ids

--- convert_bottom_up_image_features.py
+++ convert_bottom_up_image_features.py
@@ -31,34 +31,32 @@ feature_length = 2048
 num_fixed_boxes = 36
 
 
-def convert(input_file, preprocessed_data):
+def convert(input_file):
     output_filename = "features.hdf5"
     print("Saving features to {}".format(output_filename))
     output_file = h5py.File(output_filename, "w")
 
-    h5py_file = h5py.File(preprocessed_data, "r")
-    image_ids = list(h5py_file.keys())
-    print("Found {} image IDs".format(len(image_ids)))
     print("Reading tsv...")
     with open(input_file, "rt") as tsv_in_file:
         reader = csv.DictReader(tsv_in_file, delimiter="\t", fieldnames=FIELDNAMES)
+        count = 0
         for item in tqdm(reader):
             image_id = item["image_id"]
-            if image_id in image_ids:
-                item["num_boxes"] = int(item["num_boxes"])
+            item["num_boxes"] = int(item["num_boxes"])
 
-                image_features = np.frombuffer(
-                    base64.b64decode(item["features"]), dtype=np.float32
-                ).reshape((item["num_boxes"], -1))
-                output_file.create_dataset(
-                    image_id,
-                    (num_fixed_boxes, feature_length),
-                    dtype="f",
-                    data=image_features,
-                )
+            image_features = np.frombuffer(
+                base64.b64decode(item["features"]), dtype=np.float32
+            ).reshape((item["num_boxes"], -1))
+            output_file.create_dataset(
+                image_id,
+                (num_fixed_boxes, feature_length),
+                dtype="f",
+                data=image_features,
+            )
+            count += 1
 
     output_file.close()
-    print("done!")
+    print("Converted features for {} images".format(count))
 
 
 def check_args(args):
@@ -68,11 +66,6 @@ def check_args(args):
         help="TSV file with image features",
         default="../datasets/trainval_36/trainval_resnet101_faster_rcnn_genome_36.tsv",
     )
-    parser.add_argument(
-        "--preprocessed-data",
-        help="HDF5 file with preprocessed data (only used to read image COCO IDs)",
-        default="../datasets/coco2014_preprocessed/images.hdf5",
-    )
 
     parsed_args = parser.parse_args(args)
     print(parsed_args)
@@ -81,4 +74,4 @@ def check_args(args):
 
 if __name__ == "__main__":
     parsed_args = check_args(sys.argv[1:])
-    convert(parsed_args.input_tsv, parsed_args.preprocessed_data)
+    convert(parsed_args.input_tsv)
