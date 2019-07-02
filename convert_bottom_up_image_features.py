00d297e335bd24e4e20a052f61dd97ad26644d40
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2019-07-02 10:57:23 +0200

    Adapt bottom-up conversion script for multiple input TSVs

--- convert_bottom_up_image_features.py
+++ convert_bottom_up_image_features.py
@@ -30,41 +30,46 @@ FIELDNAMES = ["image_id", "image_w", "image_h", "num_boxes", "boxes", "features"
 feature_length = 2048
 
 
-def convert(input_file):
+def convert(base_dir):
     output_filename = "features.hdf5"
     print("Saving features to {}".format(output_filename))
     output_file = h5py.File(output_filename, "w")
 
-    print("Reading tsv...")
-    with open(input_file, "rt") as tsv_in_file:
-        reader = csv.DictReader(tsv_in_file, delimiter="\t", fieldnames=FIELDNAMES)
-        count = 0
-        for item in tqdm(reader):
-            image_id = item["image_id"]
-            item["num_boxes"] = int(item["num_boxes"])
-
-            image_features = np.frombuffer(
-                base64.b64decode(item["features"]), dtype=np.float32
-            ).reshape((item["num_boxes"], -1))
-            if image_id not in output_file:
-                output_file.create_dataset(
-                    image_id,
-                    (item["num_boxes"], feature_length),
-                    dtype="f",
-                    data=image_features,
+    for dir in os.listdir(base_dir):
+        if os.path.isfile(dir):
+            input_file = os.path.join(base_dir, dir)
+            print("Reading tsv: ", input_file)
+            with open(input_file, "rt") as tsv_in_file:
+                reader = csv.DictReader(
+                    tsv_in_file, delimiter="\t", fieldnames=FIELDNAMES
                 )
-                count += 1
+                count = 0
+                for item in tqdm(reader):
+                    image_id = item["image_id"]
+                    item["num_boxes"] = int(item["num_boxes"])
 
-    output_file.close()
-    print("Converted features for {} images".format(count))
+                    image_features = np.frombuffer(
+                        base64.b64decode(item["features"]), dtype=np.float32
+                    ).reshape((item["num_boxes"], -1))
+                    if image_id not in output_file:
+                        output_file.create_dataset(
+                            image_id,
+                            (item["num_boxes"], feature_length),
+                            dtype="f",
+                            data=image_features,
+                        )
+                        count += 1
+
+            output_file.close()
+            print("Converted features for {} images".format(count))
 
 
 def check_args(args):
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--input-tsv",
-        help="TSV file with image features",
-        default="../datasets/trainval_36/trainval_resnet101_faster_rcnn_genome_36.tsv",
+        help="Directory containing the TSV files with image features",
+        default="../datasets/trainval_36/",
     )
 
     parsed_args = parser.parse_args(args)
