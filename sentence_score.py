Fix sentence score calculation for butd model

sentence_score.py
@@ -17,6 +17,10 @@ from utils import (
     IMAGES_FILENAME,
     IMAGENET_IMAGES_MEAN,
     IMAGENET_IMAGES_STD,
+    MODEL_SHOW_ATTEND_TELL,
+    MODEL_BOTTOM_UP_TOP_DOWN,
+    BOTTOM_UP_FEATURES_FILENAME,
+    MODEL_BOTTOM_UP_TOP_DOWN_RANKING,
 )
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -26,14 +30,19 @@ def sequence_score(encoder, decoder, img, word_map, sequence):
     """Calculate the sequence score for a given image-sequence-pair."""
 
     # Move image to GPU device, if available
-    image = img.to(device)
+    encoder_out = img.to(device)
 
     # Encode
-    encoder_out = encoder(image)  # (1, enc_image_size, enc_image_size, encoder_dim)
-    encoder_dim = encoder_out.size(3)
+    if encoder:
+        encoder_out = encoder(
+            encoder_out
+        )  # (1, enc_image_size, enc_image_size, encoder_dim)
+        encoder_dim = encoder_out.size(3)
 
-    # Flatten encoding
-    encoder_out = encoder_out.view(1, -1, encoder_dim)  # (1, num_pixels, encoder_dim)
+        # Flatten encoding
+        encoder_out = encoder_out.view(
+            1, -1, encoder_dim
+        )  # (1, num_pixels, encoder_dim)
 
     # Encode sequence
     encoded_sequence = [word_map[token] for token in word_tokenize(sequence)]
@@ -69,12 +78,15 @@ def sequence_score(encoder, decoder, img, word_map, sequence):
 def load_model_and_calculate_score(checkpoint, data_folder, image_id, sequences):
     # Load model
     checkpoint = torch.load(checkpoint, map_location=device)
+    model_name = checkpoint["model_name"]
+
     decoder = checkpoint["decoder"]
     decoder = decoder.to(device)
     decoder.eval()
     encoder = checkpoint["encoder"]
-    encoder = encoder.to(device)
-    encoder.eval()
+    if encoder:
+        encoder = encoder.to(device)
+        encoder.eval()
 
     # Load word map
     word_map_path = os.path.join(data_folder, WORD_MAP_FILENAME)
@@ -82,12 +94,31 @@ def load_model_and_calculate_score(checkpoint, data_folder, image_id, sequences)
         word_map = json.load(json_file)
 
     # Read image and process
-    h5py_file = h5py.File(os.path.join(data_folder, IMAGES_FILENAME), "r")
+    if model_name == MODEL_SHOW_ATTEND_TELL:
+        image_features_file = IMAGES_FILENAME
+    elif (
+        model_name == MODEL_BOTTOM_UP_TOP_DOWN
+        or model_name == MODEL_BOTTOM_UP_TOP_DOWN_RANKING
+    ):
+        image_features_file = BOTTOM_UP_FEATURES_FILENAME
+    else:
+        raise NotImplementedError()
+
+    h5py_file = h5py.File(os.path.join(data_folder, image_features_file), "r")
     image_data = h5py_file[image_id].value
 
-    image = torch.FloatTensor(image_data / 255.0)
-    normalize = transforms.Normalize(mean=IMAGENET_IMAGES_MEAN, std=IMAGENET_IMAGES_STD)
-    image = normalize(image).unsqueeze(0)
+    if model_name == MODEL_SHOW_ATTEND_TELL:
+        image_data = image_data / 255.0
+
+    image = torch.FloatTensor(image_data)
+
+    if model_name == MODEL_SHOW_ATTEND_TELL:
+        normalize = transforms.Normalize(
+            mean=IMAGENET_IMAGES_MEAN, std=IMAGENET_IMAGES_STD
+        )
+        image = normalize(image)
+
+    image = image.unsqueeze(0)
 
     for sequence in sequences:
         score = sequence_score(encoder, decoder, image, word_map, sequence)